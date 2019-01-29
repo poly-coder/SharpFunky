da0689f1477454b71bbf29c1e513ad3f76ef9e7c
Iskander <iskander.sierra@gmail.com>
2019-01-29 09:41:41 +0100

    Removed servers and topics

--- servers/SharpFunky.EventServer.Grains/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace SharpFunky.EventServer.Grains
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- topics/Topics.Clients/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace Topics.Clients
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
